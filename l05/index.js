a1ced6bd97b7d2c7376c8744a00f0097e2dd32c6
jhonvi2 <juanvegaseco99@gmail.com>
2021-03-18 10:25:17 +0100

    L05

---
+++ l05/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var port = 80;
+
+app.use("/", express.static(path.join( __dirname + "./public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos
+
+app.get("/hello", (req,res) => {
+res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
+}); //configuramos la ruta y la respuesta a esa ruta 
+
+
+app.listen(port, () =>{
+    console.log('server ready listening on port ${port} ');
+}); //para arrancar el servidor
+
+app.post("/hello", (req,res) => {
+    res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
+    }); //Esto no se puede ver en en el navegador, pero se puede ver en en el postman
