5baf9a3244821acc728b8ee27c00ed7e1e802b3e
jhonvi2 <juanvegaseco99@gmail.com>
2021-03-21 11:45:10 +0100

    Cambios en mostrar el contenido statico

--- l05/index.js
+++ l05/index.js
@@ -5,7 +5,7 @@ var app = express();
 
 var port = 80;
 
-app.use("/", express.static(path.join( __dirname + "./public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos
+app.use("/", express.static(path.join( __dirname + "/public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos. Dirname coge la carpera donde se ejecuta el npm start y la encadena con /public
 
 app.get("/hello", (req,res) => {
 res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
@@ -13,7 +13,7 @@ res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
 
 
 app.listen(port, () =>{
-    console.log('server ready listening on port ${port} ');
+    console.log(`server ready listening on port ${port}`);
 }); //para arrancar el servidor
 
 app.post("/hello", (req,res) => {
