1b369cade3a932ca0430de274c2a6e43ab381b68
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2021-03-21 20:30:31 +0100

    puerto cambiado

--- l05/index.js
+++ l05/index.js
@@ -2,10 +2,10 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
+ 
+var port = 10000;
 
-var port = 80;
-
-app.use("/", express.static(path.join( __dirname + "/public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos. Dirname coge la carpera donde se ejecuta el npm start y la encadena con /public
+app.use("/", express.static(path.join( __dirname + "./public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos
 
 app.get("/hello", (req,res) => {
 res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
@@ -13,7 +13,7 @@ res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
 
 
 app.listen(port, () =>{
-    console.log(`server ready listening on port ${port}`);
+    console.log('server ready listening on port ${port} ');
 }); //para arrancar el servidor
 
 app.post("/hello", (req,res) => {
