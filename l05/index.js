dec23283c141d6dc0d87ce5d56a933a85c630140
antoniosilva096 <36417018+antoniosilva096@users.noreply.github.com>
2021-03-22 15:18:18 +0100

    modificacion l05

--- l05/index.js
+++ l05/index.js
@@ -1,20 +1,22 @@
 var express = require("express");
+
 var path = require("path");
 
 var app = express();
- 
-var port = 10000;
 
-app.use("/", express.static(path.join( __dirname + "./public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos
+var port = (process.env.PORT || 10000);
 
-app.get("/hello", (req,res) => {
-res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
-}); //configuramos la ruta y la respuesta a esa ruta 
+//app.use("/", express.static("./public")); //Valido en linux
+//app.use("/", express.static( __dirname + "\public")); //Recomendado para windows
+app.use("/", express.static(path.join(__dirname,"public"))); //Recomendado para todos los sistemas (usar este)
 
+app.get("/hello", (req, res) =>{
+res.send("<html><body><h1>Hello from this tiny server</h1></body></html>")
+});
 
-app.listen(port, () =>{
-    console.log('server ready listening on port ${port} ');
-}); //para arrancar el servidor
+app.listen(port);
+
+console.log('Server listening on port: '+port);
 
 app.post("/hello", (req,res) => {
     res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
