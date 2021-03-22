da848a3a0265be845be3a1d5bc78fcc91df6ff22
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2021-03-22 21:30:09 +0100

    subiendo a heroku

--- l05/index.js
+++ l05/index.js
@@ -5,17 +5,17 @@ var app = express();
  
 var port = 10000;
 
-app.use("/", express.static(path.join( __dirname + "./public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos
+app.use("/", express.static(path.join( __dirname + "/public"))); //busca en public y ejecuta, el metodo path.join unifica el formato de la url entre sistemas operativos
 
 app.get("/hello", (req,res) => {
-res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
+res.send("<html><body><h1> GET Hello from this tiny server </h1> </body> </html>");
 }); //configuramos la ruta y la respuesta a esa ruta 
 
 
 app.listen(port, () =>{
-    console.log('server ready listening on port ${port} ');
+    console.log(`server ready listening on port ${port} `);
 }); //para arrancar el servidor
 
 app.post("/hello", (req,res) => {
-    res.send("<html><body><h1>Hello from this tiny server </h1> </body> </html>");
+    res.send("<html><body><h1>POST Hello from this tiny server </h1> </body> </html>");
     }); //Esto no se puede ver en en el navegador, pero se puede ver en en el postman
