fa999c6e43668af9889661aa733c511d19c21836
antoniosilva096 <36417018+antoniosilva096@users.noreply.github.com>
2021-03-22 16:09:04 +0100

    Creación carpeta info dentro de l05, inserción de la ruta de dicha carpeta y creacion del recurso owid-covid-data.html

--- l05/index.js
+++ l05/index.js
@@ -9,7 +9,7 @@ var port = (process.env.PORT || 10000);
 //app.use("/", express.static("./public")); //Valido en linux
 //app.use("/", express.static( __dirname + "\public")); //Recomendado para windows
 app.use("/", express.static(path.join(__dirname,"public"))); //Recomendado para todos los sistemas (usar este)
-
+app.use("/", express.static(path.join(__dirname,"info")));
 app.get("/hello", (req, res) =>{
 res.send("<html><body><h1>Hello from this tiny server</h1></body></html>")
 });
