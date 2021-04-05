5477f167a86e5002f2873d25d3676ee4e5589e6c
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2021-04-05 17:31:01 +0200

    parte carran

--- l06/index.js
+++ l06/index.js
@@ -1,42 +1,50 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var port = process.env.port || 1607;  // port va a valer lo que valga la variable de entorno y sino lo pone en el puero 1607
-var BASE_API_PATH = "/api/v1/statewiseTestingDetails"; //tipo de recurso
+var BASE_API_PATH = "/api/v1"; //tipo de recurso
 var app = express(); //generamos aplicacion que va a hacer uso de express
 
 app.use("/", express.static("./public"));
 app.use(bodyParser.json()); //todos los json que lleguen en la peticion los transformara en objects
 
-var statewiseTestingDetails = [
+var us_counties_covid19_daily = [
     {
-        "name": "pablo",
-        "phone": 2000
-    },
+        "date": 2021-08-02,
+        "county": "Snohomish",
+        "state": "Washington",
+        "fips": 53061.0,
+        "cases": 1,
+        "deaths":0.0
+        },
     {
-        "name": "pepe",
-        "phone": 6890
-    },
+        "date": 2021-08-24,
+        "county": "Coock",
+        "state": "Illinois",
+        "fips": 17031.0,
+        "cases": 1,
+        "deaths":0.0
+    }
 
 ];
 
 
-app.get(BASE_API_PATH+ "/loadInitialData", (req,res)=>{ //cuando aquí llamen a /api/v1/contacts voy a devolver el json
-    res.send(JSON.stringify(statewiseTestingDetails,null,2)); //hay que pasar el objeto a json con stringfy pasandole el array creado, con 2 le das formato bonito
+app.get(BASE_API_PATH+ "/us_counties_covid19_daily/loadInitialData", (req,res)=>{ //cuando aquí llamen a /api/v1/contacts voy a devolver el json
+    res.send(JSON.stringify(us_counties_covid19_daily,null,2)); //hay que pasar el objeto a json con stringfy pasandole el array creado, con 2 le das formato bonito
 }) //5
 
 app.get(BASE_API_PATH+ "/stats", (req,res)=>{ //cuando aquí llamen a /api/v1/contacts voy a devolver el json
-    res.send(JSON.stringify(statewiseTestingDetails,null,2)); //hay que pasar el objeto a json con stringfy pasandole el array creado, con 2 le das formato bonito
+    res.send(JSON.stringify(us_counties_covid19_daily,null,2)); //hay que pasar el objeto a json con stringfy pasandole el array creado, con 2 le das formato bonito
 }) //6.1
 
 app.get(BASE_API_PATH+ "/stats/pablo", (req,res)=>{ //cuando aquí llamen a /api/v1/contacts voy a devolver el json
-    res.send(JSON.stringify(statewiseTestingDetails[0],null,2)); //hay que pasar el objeto a json con stringfy pasandole el array creado, con 2 le das formato bonito
+    res.send(JSON.stringify(us_counties_covid19_daily,null,2)); //hay que pasar el objeto a json con stringfy pasandole el array creado, con 2 le das formato bonito
 }) //6.2
 
 app.post(BASE_API_PATH+ "/stats", (req,res)=>{  //en este caso creamos el contacto
-    var newContact = [{"name": "Juan", "phone": 722 } ];  
+    var newContact = req.body;  
     console.log(`new contact added: <${JSON.stringify(newContact,null,2)}>`);
 
-    statewiseTestingDetails.push(newContact);
+    us_counties_covid19_daily.push(newContact);
 
     res.sendStatus(201);
 
