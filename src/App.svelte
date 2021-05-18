dc268c95b1d61a47437e993d1ff7b29709a5e5c1
antoniosilva096 <36417018+antoniosilva096@users.noreply.github.com>
2021-05-18 07:38:54 +0200

    t

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,33 @@
   
 <script lang="ts">
 	import Router from 'svelte-spa-router';
-	  
 	import NotFound from "./Pages/NotFound.svelte";
 	import Home from "./Pages/Home.svelte";
 	import Info from "./Pages/Info.svelte";
 	import covid19TrackingGermanyAPI from "./front/antsilgorAPI/covid19TrackingGermanyAPI.svelte";
 	import EditCovid19TrackingGermanyAPI from "./front/antsilgorAPI/EditCovid19TrackingGermanyAPI.svelte";
-	 
 	import statewisetestingdetailsAPI from "./front/juavegsecAPI/statewisetestingdetailsAPI.svelte";
 	import Editstatewisetestingdetails from "./front/juavegsecAPI/Editstatewisetestingdetails.svelte";
-
 	import us_counties_covid19API  from "./front/antcarbarAPI/us_counties_covid19API.svelte";
 	import EditUs_counties_covid19 from "./front/antcarbarAPI/EditUs_counties_covid19.svelte";
+    import GraficaCovid19TrackingGermany from './front/antsilgorAPI/graphs/graficaCovid19TrackingGermany.svelte';
 
 	  const routes = {
 		  "/":Home,
 		  "/info":Info,
 		  "/covid19-tracking-germany": covid19TrackingGermanyAPI,
+		  "/covid19-tracking-germany/analytics": GraficaCovid19TrackingGermany,
 		  "/covid19-tracking-germany/:state/:county":EditCovid19TrackingGermanyAPI,
 		  "/statewisetestingdetails": statewisetestingdetailsAPI,
 		  "/statewisetestingdetails/:date/:state":Editstatewisetestingdetails,
 		  "/us_counties_covid19_daily":  us_counties_covid19API,
 		  "/us_counties_covid19_daily/:county/:fips":EditUs_counties_covid19,
-		  '*': NotFound};
+		  '*': NotFound
+		};
   </script>
   <main>
       <Router {routes}> </Router>
   </main>
-  
   <style>
   </style>
+
