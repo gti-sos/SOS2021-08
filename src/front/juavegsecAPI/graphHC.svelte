83c9fb9899d38d5838f948d840b209a37e40a4cf
jhonvi2 <juanvegaseco99@gmail.com>
2021-05-27 12:19:01 +0200

    cuarto arreglo DESESPERACION

--- src/front/juavegsecAPI/graphHC.svelte
+++ src/front/juavegsecAPI/graphHC.svelte
@@ -90,6 +90,7 @@ async function getData(){
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script src="https://code.highcharts.com/highcharts.src.js" on:load="{loadGraph}"></script>
  
 </svelte:head>
