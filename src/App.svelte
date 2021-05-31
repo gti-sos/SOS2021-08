844307a7b49eefda7d824b78b9007303e5a692d8
antoniosilva096 <36417018+antoniosilva096@users.noreply.github.com>
2021-05-31 13:42:41 +0200

    prueba merge

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,20 @@
   
 <script lang="ts">
 	import Router from 'svelte-spa-router';
+
 	import NotFound from "./Pages/NotFound.svelte";
 	import Home from "./Pages/Home.svelte";
 	import Info from "./Pages/Info.svelte";
 	import covid19TrackingGermanyAPI from "./front/antsilgorAPI/covid19TrackingGermanyAPI.svelte";
 	import EditCovid19TrackingGermanyAPI from "./front/antsilgorAPI/EditCovid19TrackingGermanyAPI.svelte";
-	import GraficaCovid19TrackingGermany from './front/antsilgorAPI/graphs/graficaCovid19TrackingGermany.svelte';
+	
 	import statewisetestingdetailsAPI from "./front/juavegsecAPI/statewisetestingdetailsAPI.svelte";
 	import Editstatewisetestingdetails from "./front/juavegsecAPI/Editstatewisetestingdetails.svelte";
+	import graph from "./front/juavegsecAPI/graphHC.svelte";
+	import graph2 from "./front/juavegsecAPI/graphHC2.svelte";
+	
 	import us_counties_covid19API  from "./front/antcarbarAPI/us_counties_covid19API.svelte";
 	import EditUs_counties_covid19 from "./front/antcarbarAPI/EditUs_counties_covid19.svelte";
-    
 
 	  const routes = {
 		  "/":Home,
@@ -23,8 +26,10 @@
 		  "/statewisetestingdetails/:date/:state":Editstatewisetestingdetails,
 		  "/us_counties_covid19_daily":  us_counties_covid19API,
 		  "/us_counties_covid19_daily/:county/:fips":EditUs_counties_covid19,
-		  '*': NotFound
-		};
+
+		  "/statewisetestingdetails/graph": graph,
+		  "/statewisetestingdetails/graph2": graph2,
+		  '*': NotFound};
   </script>
   <main>
       <Router {routes}> </Router>
