388d52fcff8a2046d3cc452a064065fcd95232e4
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2021-06-07 19:00:57 +0200

    Grafica nueva carran

--- src/App.svelte
+++ src/App.svelte
@@ -24,8 +24,9 @@
 	import us_counties_covid19API  from "./front/antcarbarAPI/us_counties_covid19API.svelte";
 	import EditUs_counties_covid19 from "./front/antcarbarAPI/EditUs_counties_covid19.svelte";
 	import graphicDonut3D from './front/antcarbarAPI/graphicDonut3D.svelte';
-	import chartist from './front/antcarbarAPI/chartist.svelte';
-
+	import chartist from './front/antcarbarAPI/chartist.svelte';	
+	import highchartBB from './front/antcarbarAPI/highchartBB.svelte';
+	import fusionchart1 from './front/antcarbarAPI/fusionchart1.svelte';
 
 	
 
@@ -42,6 +43,8 @@
 		  "/us_counties_covid19_daily/:county/:fips":EditUs_counties_covid19,
 		  "/us_counties_covid19_daily/graphicDonut3D":graphicDonut3D,
 		  "/us_counties_covid19_daily/chartist":chartist,
+		  "/us_counties_covid19_daily/fusionchart1":fusionchart1,
+		  "/us_counties_covid19_daily/highchartBB":highchartBB,
 		  "/statewisetestingdetails/graph2": graph2,
 		  "/statewisetestingdetails/amcChart": graphAMC,
 		  "/statewisetestingdetails/integrationAnx": graphintegrationAnx,
