48b899be0381aedff8ea4ab03d74192842d83829
Antonio Carranza Barroso <49164757+carran97@users.noreply.github.com>
2021-06-08 11:00:40 +0200

    video

--- src/Pages/aboutus.svelte
+++ src/Pages/aboutus.svelte
@@ -22,7 +22,8 @@
         <main>
             <h3>Video explicativo de interfaz de usuario y backend de los Positivos,negativos y casos totales en India - Juan Vega Seco</h3>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/uElwbed8sN8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
+            <h3>Video explicativo de interfaz de usuario y backend de casos de covid  en EEUU- Antonio Carranza Barroso</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/AOJilfL84EI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         
         </main>
     </body>
